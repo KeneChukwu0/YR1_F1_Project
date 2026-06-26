5fe7b3f7bd6ae258e405cdc2a0229eb9008b0a99
psyjc33 <144380895+KeneChukwu0@users.noreply.github.com>
2026-06-26 11:02:45 +0100

    Now can fetch any session from 2018 onwards

---
+++ getsession.py
@@ -0,0 +1,8 @@
+import fastf1
+import pandas as pd
+year = int(input("Select Year \n"))
+race = input("Select race \n")
+weekendsession = input("Select session \n")
+session = fastf1.get_session(year, race,weekendsession)
+session.load()
+print(session.results.iloc[0:20].loc[:, ['Abbreviation', 'TeamName','ClassifiedPosition']])
